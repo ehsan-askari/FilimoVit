ad0e89980030ef9b7063b46dc6a8e9d59792a7d6
Ehsan Askari <ehsanaskari74@gmail.com>
2020-07-25 02:08:27 +0430

    Add SomeTest

--- FilimoVitTests/FilimoVitTests.swift
+++ FilimoVitTests/FilimoVitTests.swift
@@ -10,25 +10,43 @@ import XCTest
 @testable import FilimoVit
 
 class FilimoVitTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testAPI() {
+        APIManager.request(endpoint: "/movie/movie/list/tagid/1", httpMethod: .get) { (result) in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-
+    
+    func testAPIEndpoint() {
+        APIManager.request(endpoint: "/movie/movi/lst/tad/1", httpMethod: .get) { (result) in
+            switch result {
+            case .success(let data):
+                print(data)
+            case .failure(let error):
+                print(error)
+                XCTAssertEqual(error.localizedDescription, "notFound")
+            }
+        }
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
